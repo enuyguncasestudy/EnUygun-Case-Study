21909854d348a8cc74606b7de368725c575f08ed
Anıl Sözeri <aS@Anils-MacBook-Pro.local>
2017-10-05 00:07:35 +0300

    Initial Commit

---
+++ EnUygun Case Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnUygun Case Study
+//
+//  Created by Anıl Sözeri on 05/10/2017.
+//  Copyright © 2017 Anıl Sözeri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
